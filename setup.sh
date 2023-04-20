6797f136c5d75579860a5a6b0960842b783ae91e
Seanghay Yath <seanghay.dev@gmail.com>
2023-04-20 18:55:51 +0700

    chore: add echo

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+echo "1. Check for GPU"
 nvidia-smi
 
+echo "2. Install Git LFS"
 sudo apt-get update -y
 sudo apt-get install git-lfs -y
 
+echo "3. Create venv"
 python3 -m venv hf_env
 source hf_env/bin/activate
 echo "source ~/hf_env/bin/activate" >> ~/.bashrc
 
+echo "4. Install deps"
 pip install -r requirements.txt
 
+echo "5. Authenticate"
 git config --global credential.helper store
 huggingface-cli login
 
